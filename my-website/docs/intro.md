9cf8dda5880288e9793397ab79ddb1ed979f5394
Diana <ddvoryadckina@yandex.ru>
2024-12-04 23:03:00 +0300

    commit

--- my-website/docs/intro.md
+++ my-website/docs/intro.md
@@ -2,7 +2,7 @@
 sidebar_position: 1
 ---
 
-# Bilingual App
+# Introduction
 
 Let's discover **Docusaurus in less than 5 minutes**.
 
--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -98,10 +98,6 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
                 label: 'GitHub',
                 href: 'https://github.com/DvoryadkinaDV',
               },
